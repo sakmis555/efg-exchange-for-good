d393801e1f655d6a9c5c93bfeec687e6ff6272c5
Saksham Mishra <sakmis555@gmail.com>
2023-05-31 06:18:36 +0530

    ProtectionPage Layout added

--- frontEnd/src/components/ProtectedPage.js
+++ frontEnd/src/components/ProtectedPage.js
@@ -17,27 +17,53 @@ function ProtectedPage({ children }) {
         message.error(response.message);
       }
     } catch (error) {
-        navigate("/login");
+      navigate("/login");
       message.error(error.message);
     }
   };
 
   useEffect(() => {
-    if(localStorage.getItem("token")) {
-        validateToken();
+    if (localStorage.getItem("token")) {
+      validateToken();
     } else {
-        navigate("/login");
+      navigate("/login");
     }
   }, []);
   return (
-    <div>
-      {user && (
+    user && (
+      <div>
+        {/* header of every protected page */}
+        <div className="flex items-center justify-between bg-primary p-6"> 
+          <h1 className="text-2xl text-whiteLike pl-3">
+            EFG
+          </h1>
+          <div className="bg-whiteLike py-2 px-5 rounded flex gap-2 items-center">
+          <i className="ri-user-4-fill"></i>
+            <span className="underline cursor-pointer uppercase">
+              {user.name}
+            </span>
+            <i className="ri-logout-box-r-fill ml-10 cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.href = "/";
+                // navigate("/");
+              }}
+            ></i>
+          </div>
+        </div>
+
+        {/* Body */}
         <div className="p-5">
           {user.name}
           {children}
         </div>
-      )}
-    </div>
+        
+        {/* Footer : will do after writting the different pages*/}
+        {/* <div className="flex items-center justify-center bg-primary p-3"> 
+
+        </div> */}
+      </div>
+    )
   );
 }
 
