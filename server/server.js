24e03e20cd5388ad2f770aeeb0c543ccb43afbba
Saksham Mishra <sakmis555@gmail.com>
2023-05-31 23:45:16 +0530

    Product api (productRoute.js) and productModel.js added

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-const dotenv = require('dotenv');
 
+const dotenv = require('dotenv');
 dotenv.config({path: "../.env"});
 const dbConfig = require("./config/dbConfig");
+
 const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/usersRoute");
+const productRoute = require("./routes/productsRoute");
+
 app.use("/api/users", usersRoute);
+app.use("/api/products", productRoute);
 
 app.listen(port, () => console.log(`Node/express Server is running on port ${port}`));
