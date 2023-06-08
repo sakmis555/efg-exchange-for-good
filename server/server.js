8e9bd68dd1eccbe04dfa6eef45ec72d3dae70005
Saksham Mishra <sakmis555@gmail.com>
2023-06-08 21:11:05 +0530

    deployment setup

--- server/server.js
+++ server/server.js
@@ -19,4 +19,15 @@ app.use("/api/products", productRoute);
 app.use("/api/bids", bidsRoute);
 app.use("/api/notifications", notificationRoute);
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontEnd/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontEnd", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Node/express Server is running on port ${port}`));
